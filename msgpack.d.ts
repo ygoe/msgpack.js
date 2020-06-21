fc24b92e38051d85f555861aa4f0908c66f476f9
Yves Goergen <git@ygoe.de>
2020-06-21 12:51:36 +0200

    Updated TypeScript declarations for new 'multiple' parameters (#13)

--- msgpack.d.ts
+++ msgpack.d.ts
@@ -1,14 +1,16 @@
 /**
  * Serializes a value to a MessagePack byte array.
- * @param data The value to serialize. This can be a scalar, array or object
+ * @param data The value to serialize. This can be a scalar, array or object.
+ * @param multiple Indicates whether multiple values in data are concatenated to multiple MessagePack arrays.
  */
-export function serialize(data: any): Uint8Array;
+export function serialize(data: any, multiple: boolean = false): Uint8Array;
 
 /**
  * Deserializes a MessagePack byte array to a value.
  * @param array The MessagePack byte array to deserialize.
+ * @param multiple Indicates whether multiple concatenated MessagePack arrays are returned as an array.
  */
-export function deserialize(array: Uint8Array | ArrayBuffer | []): any;
+export function deserialize(array: Uint8Array | ArrayBuffer | [], multiple: boolean = false): any;
 
 export { serialize as encode };
 export { deserialize as decode };
